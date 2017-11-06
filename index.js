78754dde61f592991c03758b38b1d9040377f99a
Daniel Kerr <lorddanp@aol.com>
2017-11-06 21:26:49 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,12 @@
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts (string[] x) {
+  for (int i = 0; i < x.size; i++;) {
+    x[i] = `${i} +!!!`
+  }
+}
